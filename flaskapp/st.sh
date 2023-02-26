c8dce28d1d7a5fc89d6a029cab9f95b6682740aa
ilyazhokhovets <43029511+ilyazhokhovets@users.noreply.github.com>
2023-02-27 01:47:33 +0300

    Update st.sh

--- flaskapp/st.sh
+++ flaskapp/st.sh
@@ -1,15 +1,10 @@
 gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
-sleep 5
-<<<<<<< HEAD
+sleep 25
 echo start client
 python3 client.py
+APP_CODE=$?
 sleep 5
 echo $APP_PID
 kill -TERM $APP_PID
-exit 0
-=======
-echo $APP_PID
-kill -TERM $APP_PID
-echo process gunicorns kills
-exit 0
->>>>>>> origin/main
+echo app code $APP_CODE
+exit $APP_CODE
